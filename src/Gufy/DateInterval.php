c4cae4414c9463351f57a59e0cdda4912a7b7dd6
Mochamad Gufron <mgufronefendi@gmail.com>
2013-11-13 01:20:33 +0700

    modify methods

--- src/Gufy/DateInterval.php
+++ src/Gufy/DateInterval.php
@@ -16,14 +16,22 @@ class DateInterval
 	* @var int $interval timestamp interval
 	*/
 	public $interval=0;
+
 	/**
 	* @var int $interval save interval when using format mode
 	*/
 	private $tempInterval=0;
+
+	/**
+	* @var string $format used format for forming the output
+	*/
+	public $format = '';
+
 	/**
 	* @param int $interval timestamp interval
 	* @param DateTime $interval the first date 
 	* @param DateTime $last_date the last date so we can find the interval of the first and the last date 
+	* @return self object
 	*/
 	public function __construct($interval=0,\DateTime $last_date=null)
 	{
@@ -34,34 +42,62 @@ class DateInterval
 			$interval = $last_interval - $first_interval;
 		}
 		$this->interval = $interval;
+		return $this;
+	}
+
+	/**
+	* @param int $interval set current time interval
+	* @return self object
+	*/
+	public function setInterval($interval)
+	{
+		$this->interval = $interval;
+		return $this;
+	}
+
+	/**
+	* @param string $format set current format
+	* @return self object
+	*/
+	public function setFormat($format)
+	{
+		$this->format = $format;
+		return $this;
 	}
 
 	/**
 	* @param int $round_method rounding method after we reveal the readable interval. You can use PHP_ROUND_HALF_UP and PHP_ROUND_HALF_DOWN
+	* @return float in seconds
 	*/
 	public function getSeconds($round_method=null)
 	{
 		$result = $this->interval/1000;
 		return $this->round($result,$round_method);
 	}
+
 	/**
 	* @param int $round_method rounding method after we reveal the readable interval. You can use PHP_ROUND_HALF_UP and PHP_ROUND_HALF_DOWN
+	* @return float in minutes
 	*/
 	public function getMinutes($round_method=null)
 	{
-		$result = ($this->interval/1000)/60;
+		$result = $this->interval/(1000*60);
 		return $this->round($result,$round_method);
 	}
+
 	/**
 	* @param int $round_method rounding method after we reveal the readable interval. You can use PHP_ROUND_HALF_UP and PHP_ROUND_HALF_DOWN
+	* @return float in hours
 	*/
 	public function getHours($round_method=null)
 	{
-		$result = (($this->interval/1000)/60)/60;
+		$result = $this->interval/(1000*60*60);
 		return $this->round($result,$round_method);
 	}
+
 	/**
 	* @param string $format rounding method after we reveal the readable interval. You can use PHP_ROUND_HALF_UP and PHP_ROUND_HALF_DOWN
+	* @return well formatted output
 	*/
 	public function format($format)
 	{
@@ -82,8 +118,10 @@ class DateInterval
 		));
 		return $result;
 	}
+
 	/**
 	* @param int $round_method rounding method after we reveal the readable interval. You can use PHP_ROUND_HALF_UP and PHP_ROUND_HALF_DOWN
+	* @return float current number of the result. if it is using $round_method, it will rounded up or down
 	*/
 	private function round($result, $round_method=null)
 	{
@@ -92,5 +130,13 @@ class DateInterval
 		elseif($round_method===PHP_ROUND_HALF_DOWN)
 			$result = floor($result);
 		return $result;
+	}
+
+	/**
+	* @return well formatted output
+	*/
+	public function __toString()
+	{
+		return $this->format($this->format);
 	}
 }
